17dc21951451313da286f3513bd35c4a3f8cbbb0
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-11-17 21:01:32 +0800

    Added time picker component

--- forms_script.js
+++ forms_script.js
@@ -1,65 +1,10 @@
-// const monthYearEl = document.querySelector("#monthYear");
-// const datesEl = document.querySelector(".dates");
-// const calPrevBtn = document.querySelector("#calPrevBtn");
-// const calNextBtn = document.querySelector("#calNextBtn");
-
-// let currentDate = new Date();
-
-// const updateCalendar = () => {
-// 	const currentYear = currentDate.getFullYear();
-// 	const currentMonth = currentDate.getMonth();
-
-// 	const firstDay = new Date(currentYear, currentMonth, 1);
-// 	const lastDay = new Date(currentYear, currentMonth + 1, 0);
-// 	const totalDays = lastDay.getDate();
-// 	const firstDayIndex = firstDay.getDay();
-// 	const lastDayIndex = lastDay.getDay();
-
-// 	const monthYearString = currentDate.toLocaleString("default", {
-// 		month: "long",
-// 		year: "numeric",
-// 	});
-// 	monthYearEl.textContent = monthYearString;
-
-// 	let datesHTML = "";
-
-// 	for (let i = firstDayIndex; i > 1; i--) {
-// 		const prevDate = new Date(currentYear, currentMonth, 0 - i + 1);
-// 		datesHTML += `<div class='date inactive'>${prevDate.getDate()}</div>`;
-// 	}
-
-// 	for (let i = 1; i <= totalDays; i++) {
-// 		const date = new Date(currentYear, currentMonth, i);
-// 		const activeClass =
-// 			date.toDateString() === currentDate.toDateString() ? "active" : "";
-// 		datesHTML += `<div class='date ${activeClass}">${i}</div>`;
-// 	}
-
-// 	for (let i = 1; i <= 7 - lastDayIndex; i++) {
-// 		const nextDate = new Date(currentYear, currentMonth + 1, i);
-// 		datesHTML += `<div class="date inactive">${nextDate.getDate()}</div>`;
-// 	}
-
-// 	datesEl.innerHTML = datesHTML;
-// };
-
-// calPrevBtn.addEventListener("click", () => {
-// 	currentDate.setMonth(currentDate.getMonth() - 1);
-// 	updateCalendar();
-// });
-// calNextBtn.addEventListener("click", () => {
-// 	currentDate.setMonth(currentDate.getMonth() + 1);
-// 	updateCalendar();
-// });
-
-// updateCalendar();
-
 const monthYearEl = document.querySelector("#monthYear");
 const datesEl = document.querySelector(".dates");
 const calPrevBtn = document.querySelector("#calPrevBtn");
 const calNextBtn = document.querySelector("#calNextBtn");
 
 let currentDate = new Date();
+let chosenDate;
 
 const updateCalendar = () => {
 	const currentYear = currentDate.getFullYear();
@@ -120,8 +65,17 @@ const getActiveDates = () => {
 };
 
 const getDayVal = (e) => {
-	console.log(e.target);
-	console.log(e.target.querySelector("[data-month-key]"));
+	console.log(
+		`Selected Month is: ${currentDate.getMonth()} Selected Day is: ${
+			e.target.innerHTML
+		} Selected Year is : ${currentDate.getFullYear()}`,
+	);
+	chosenDate = new Date(
+		currentDate.getFullYear(),
+		currentDate.getMonth(),
+		Number(e.target.innerHTML),
+	);
+	console.log(chosenDate);
 };
 
 updateCalendar();
